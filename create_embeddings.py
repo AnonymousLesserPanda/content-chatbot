84ec45f1bd9ba548173e464f666c95e71a08c039
RedPanda <001213YZL@stu.xjtu.edu.cn>
2025-02-21 15:23:15 +0800

    finish embeddings

--- create_embeddings.py
+++ create_embeddings.py
@@ -1,17 +1,15 @@
 import argparse
-import pickle
 import requests
-import xmltodict
 from bs4 import BeautifulSoup
-from langchain.vectorstores import FAISS
-from langchain.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_openai import AzureOpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 
 
 def clean_html(html_content):
     soup = BeautifulSoup(html_content, "html.parser")
-    text = soup.get_text(separator=' ')
-    return ' '.join(text.split())
+    text = soup.get_text(separator=" ")
+    return " ".join(text.split())
 
 
 def extract_text_from(url):
@@ -20,46 +18,46 @@ def extract_text_from(url):
     text = soup.get_text()
 
     lines = (line.strip() for line in text.splitlines())
-    return '\n'.join(line for line in lines if line)
+    return "\n".join(line for line in lines if line)
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Embedding website content')
-    parser.add_argument('-m', '--mode', type=str, choices=['sitemap', 'zendesk'], default='sitemap',
-                        help='Mode for data extraction: sitemap or zendesk')
-    parser.add_argument('-s', '--sitemap', type=str, required=False,
-                        help='URL to your sitemap.xml', default='https://www.paepper.com/sitemap.xml')
-    parser.add_argument('-f', '--filter', type=str, required=False,
-                        help='Text which needs to be included in all URLs which should be considered',
-                        default='https://www.paepper.com/blog/posts')
-    parser.add_argument('-z', '--zendesk', type=str, required=False,
-                        help='URL to your zendesk api')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Embedding website content")
+
+    parser.add_argument(
+        "-z",
+        "--zendesk",
+        type=str,
+        required=False,
+        default="https://support.strikingly.com/api/v2/help_center/en-us/articles.json",
+        help="URL to your zendesk api",
+    )
     args = parser.parse_args()
 
-    if args.mode == 'sitemap':
-        r = requests.get(args.sitemap)
-        xml = r.text
-        raw = xmltodict.parse(xml)
-
-        pages = []
-        for info in raw['urlset']['url']:
-            # info example: {'loc': 'https://www.paepper.com/...', 'lastmod': '2021-12-28'}
-            url = info['loc']
-            if args.filter in url:
-                pages.append({'text': extract_text_from(url), 'source': url})
-    else:  # args.mode == 'zendesk'
-        r = requests.get(args.zendesk)
-        articles = r.json().get('articles', [])
-        pages = [{"text": clean_html(article['body']), "source": article['html_url']} for article in articles]
+    r = requests.get(args.zendesk)
+    articles = r.json().get("articles", [])
+    pages = [
+        {"text": clean_html(article["body"]), "source": article["html_url"]}
+        for article in articles
+    ]
 
     text_splitter = CharacterTextSplitter(chunk_size=1500, separator="\n")
     docs, metadatas = [], []
     for page in pages:
-        splits = text_splitter.split_text(page['text'])
+        splits = text_splitter.split_text(page["text"])
         docs.extend(splits)
-        metadatas.extend([{"source": page['source']}] * len(splits))
+        metadatas.extend([{"source": page["source"]}] * len(splits))
         print(f"Split {page['source']} into {len(splits)} chunks")
 
-    store = FAISS.from_texts(docs, OpenAIEmbeddings(), metadatas=metadatas)
-    with open("faiss_store.pkl", "wb") as f:
-        pickle.dump(store, f)
+    store = FAISS.from_texts(
+        docs,
+        AzureOpenAIEmbeddings(
+            openai_api_key="3TYDIcyQ3pFOZV8hJdhjajPXfTapwF8e0sCSg580Cetrhic9OhnTJQQJ99BBACHYHv6XJ3w3AAAAACOGYmHV",
+            azure_endpoint="https://ai-redpandadeployment385863590335.openai.azure.com/",
+            deployment="text-embedding-3-large",
+            openai_api_version="2023-05-15",
+        ),
+        metadatas=metadatas,
+    )
+
+    store.save_local("faiss_store")
